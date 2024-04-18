fa8ca96f825ca55b1e91be586f86926492facdfd
RotatingCloud <113541115+RotatingCloud@users.noreply.github.com>
2024-04-17 22:01:23 -0700

    Add schedule_clean_assistants function to clean OpenAI assistants

--- src/jobs/jobs.py
+++ src/jobs/jobs.py
@@ -1,7 +1,10 @@
 from django.conf import settings
 import os
+from openai import OpenAI
+from dotenv import load_dotenv
+load_dotenv()
 
-def schedule_clean():
+def schedule_clean_audio_files():
     print("[APSCHEDULER] CLEANING MEDIA FILES")
     media_dir = "media"
     media_dir = os.path.join(settings.BASE_DIR, media_dir)
@@ -14,3 +17,12 @@ def schedule_clean():
                 if os.path.isfile(file_path):
                     os.remove(file_path)
             os.rmdir(dir_path)
+
+def schedule_clean_assistants():
+    print("[APSCHEDULER] CLEANING ASSISTANTS")
+    client = OpenAI(api_key=os.getenv('OPENAI_SECRET_KEY'))
+    assistants = client.best.assistants.list()
+    for assistant in assistants:
+        print(f"[APSCHEDULER] Deleting assistant {assistant['id']}")
+        response = client.beta.assistants.delete(assistant['id'])
+        print("[APSCHEDULER] ",  response)
--- src/jobs/updater.py
+++ src/jobs/updater.py
@@ -1,9 +1,10 @@
 from datetime import datetime
 from apscheduler.schedulers.background import BackgroundScheduler
-from .jobs import schedule_clean
+from .jobs import schedule_clean_audio_files, schedule_clean_assistants
 
 def start():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(schedule_clean, 'interval', hours=24)
+    scheduler.add_job(schedule_clean_audio_files, 'interval', hours=24)
+    scheduler.add_job(schedule_clean_assistants, 'interval', minutes=3)
     scheduler.start()
     print("[APSCHEDULER] BCS Started")
