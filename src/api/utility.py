9fb68f8760f098fbfea5b5387aafaf90bc0c96bc
RotatingCloud <113541115+RotatingCloud@users.noreply.github.com>
2024-04-30 02:55:26 -0700

    Refactor generate_list function to use wonderwords library for generating words and fix regex pattern in utility.py

--- src/api/utility.py
+++ src/api/utility.py
@@ -27,7 +27,7 @@ def generate_list():
     # # Return the list of words
     # return [word.strip().lower() for word in word_list]
     rw = RandomWord()
-    list = rw.random_words(5, word_min_length=10, word_max_length=15, include_parts_of_speech=["noun", "adjective"], regex='^\w+$')
+    list = rw.random_words(5, word_min_length=10, word_max_length=15, include_parts_of_speech=["noun", "adjective"], regex='^\w+[^s]$')
     list = [each.replace("-","") for each in list]
     # regex search for "sation" and replace with "zation"
     list = [each.replace("sation", "zation") for each in list]
