dd08f71ac5897762d4a5ae4efb3b325d9bc9760e
RotatingCloud <113541115+RotatingCloud@users.noreply.github.com>
2024-04-29 22:59:52 -0700

    Refactor generate_list function to use wonderwords library for generating words in utility.py

--- src/api/utility.py
+++ src/api/utility.py
@@ -1,27 +1,33 @@
 import openai
 import os
 from rest_framework.response import Response
+from wonderwords import RandomWord
 
 def generate_list():
-    # Initialize the OpenAI client with your API key
-    client = openai.OpenAI(api_key=os.getenv('OPENAI_SECRET_KEY'))
+    # # Initialize the OpenAI client with your API key
+    # client = openai.OpenAI(api_key=os.getenv('OPENAI_SECRET_KEY'))
 
-    # Create a completion
-    response = client.chat.completions.create(
-        model="gpt-4-turbo",
-        messages=[
-            {"role": "system", "content": "Generate a list of ONLY FIVE REAL words separated by commas. Generate a list of five words from scratch. Words should minimum 3 syllables. Do not give any comments or feedback."},
-        ]
-    )
+    # # Create a completion
+    # response = client.chat.completions.create(
+    #     model="gpt-4-turbo",
+    #     messages=[
+    #         {"role": "system", "content": "Generate a list of ONLY FIVE REAL words separated by commas. Generate a list of five words from scratch. Words should minimum 3 syllables. Do not give any comments or feedback."},
+    #     ]
+    # )
 
-    print(response)
+    # print(response)
 
-    # Extract the text from the completion object
-    words = response.choices[0].message.content
+    # # Extract the text from the completion object
+    # words = response.choices[0].message.content
 
-    print(words)
-    # Split the words by commas
-    word_list = words.replace(".", "").split(',')
+    # print(words)
+    # # Split the words by commas
+    # word_list = words.replace(".", "").split(',')
 
-    # Return the list of words
-    return [word.strip().lower() for word in word_list]
+    # # Return the list of words
+    # return [word.strip().lower() for word in word_list]
+    list = []
+    rw = RandomWord()
+    for i in range(5):
+        list.append(rw.word(syllables=(3, 3)))
+    return list
--- src/build.sh
+++ src/build.sh
@@ -4,6 +4,3 @@ set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
-
-# Apply any outstanding database migrations
-python manage.py migrate
